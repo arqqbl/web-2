2a70e867b0236deb0ad3c52dc6f944946ede24a8
arqqbl <ariqgokil333@gmail.com>
2025-02-24 11:42:18 +0700

    praktikum01

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+//mendefinisikan variabel
+$nama = 'Huda';
+$umur = 19 ;
+$berat = 60.68;
+
+echo 'Nama saya '. $nama;
+echo '<br/>Umur : '. $umur . ' tahun';
+echo '<br/>Berat : '. $berat;
+?>
